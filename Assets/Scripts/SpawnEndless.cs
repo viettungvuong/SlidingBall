92e89798e743213141db9bf1ac67c636f355eda0
viettung <viettungvuong@gmail.com>
2022-06-19 00:30:14 +0700

    Chinh input

--- Assets/Scripts/SpawnEndless.cs
+++ Assets/Scripts/SpawnEndless.cs
@@ -6,25 +6,55 @@ public class SpawnEndless : MonoBehaviour
 {
     public GameObject surface;
     Vector3 spawnPos;
-    private void Awake()
-    {
-    }
-    void Start()
-    {
-        spawnPos = transform.parent.lossyScale; //lay diem cuoi
-        
-    }
+    private float dist;
+    private bool dragging = false;
+    private Vector3 offset;
+    private Transform toDrag;
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
 
-    private void OnTriggerEnter(Collider other)
-    {
-            GameObject obstacle = Instantiate(surface);
-            obstacle.transform.position = new Vector3(spawnPos.x,transform.parent.position.y,transform.parent.position.z) ;
-            obstacle.SetActive(true);
+        Vector3 v3;
+
+        if (Input.touchCount != 1)
+        {
+            dragging = false;
+            return;
+        }
+
+        UnityEngine.Touch touch = Input.touches[0];
+        Vector3 pos = touch.position;
+
+        if (touch.phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(pos);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "Player")
+                {
+                    toDrag = hit.transform;
+                    dist = hit.transform.position.z - Camera.main.transform.position.z;
+                    v3 = new Vector3(pos.x, pos.y, dist);
+                    v3 = Camera.main.ScreenToWorldPoint(v3);
+                    offset = toDrag.position - v3;
+                    dragging = true;
+                }
+            }
+        }
+
+        if (dragging && touch.phase == TouchPhase.Moved)
+        {
+            v3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
+            v3 = Camera.main.ScreenToWorldPoint(v3);
+            toDrag.position = v3 + offset;
+        }
+
+        if (dragging && (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled))
+        {
+            dragging = false;
+        }
     }
 }
