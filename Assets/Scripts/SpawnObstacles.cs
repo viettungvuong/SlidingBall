654a1d306b81b7f43819576d2eed8da739353365
viettung <viettungvuong@gmail.com>
2022-06-18 20:29:47 +0700

    Them script spawn obstacle

---
+++ Assets/Scripts/SpawnObstacles.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class SpawnObstacles : MonoBehaviour
+{
+    const float y = 0.47f;
+    float startX = 13.41f;
+    float z;
+    void Start()
+    {
+    
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnObject();
+        startX += 4f;
+    }
+
+    void spawnObject()
+    {
+        GameObject obstacle = ObjectPool.instance.getGameObject();
+        if (obstacle != null)
+        {
+            obstacle.transform.position = new Vector3(startX, y, getZ());
+            obstacle.SetActive(true);
+        }
+    }
+
+    float getZ() //tim vi tri ngang
+    {
+        return Random.Range(-11.15f, -3.03f);
+    }
+ 
+}
