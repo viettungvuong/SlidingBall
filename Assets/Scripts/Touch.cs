4e7e02b95ad69535ad7058c672ffe358169f29a0
viettung <viettungvuong@gmail.com>
2022-06-19 20:35:46 +0700

    Update di chuyen

--- Assets/Scripts/Touch.cs
+++ Assets/Scripts/Touch.cs
@@ -4,68 +4,30 @@ using UnityEngine;
 
 public class Touch : MonoBehaviour
 {
-    const float lowerBound = -11.15f;
-    const float upperBound = -3.03f;
-    public GameObject player;
-    int move;
-    float x1, x2;
     public float speed;
-    void Start()
+    public GameObject player;
+    float halfScreenWidth;
+
+    private void Awake()
     {
+        halfScreenWidth = Screen.width * 0.5f;
     }
-
-    // Update is called once per frame
-        public GameObject surface;
-    Vector3 spawnPos;
-    private float dist;
-    private bool dragging = false;
-    private Vector3 offset;
-    private Transform toDrag;
-
     // Update is called once per frame
     void Update()
     {
-
-        Vector3 v3;
-
-        if (Input.touchCount != 1)
+        if (Input.touchCount > 0)
         {
-            dragging = false;
-            return;
-        }
-
-        UnityEngine.Touch touch = Input.touches[0];
-        Vector3 pos = touch.position;
-
-        if (touch.phase == TouchPhase.Began)
-        {
-            Ray ray = Camera.main.ScreenPointToRay(pos);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit))
+            UnityEngine.Touch touch = Input.touches[0];
+            if (touch.position.x > halfScreenWidth)
             {
-                if (hit.collider.tag == "Player")
-                {
-                    toDrag = hit.transform;
-                    dist = hit.transform.position.z - Camera.main.transform.position.z;
-                    v3 = new Vector3(pos.x, pos.y, dist);
-                    v3 = Camera.main.ScreenToWorldPoint(v3);
-                    offset = toDrag.position - v3;
-                    dragging = true;
-                }
+                Debug.Log("Left");
+                player.transform.position=Vector3.Lerp(player.transform.position, player.transform.position + new Vector3(0,0,speed*Time.deltaTime),1);
             }
-        }
-
-        if (dragging && touch.phase == TouchPhase.Moved)
-        {
-            v3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
-            v3 = Camera.main.ScreenToWorldPoint(v3);
-            toDrag.position = v3 + offset;
-        }
-
-        if (dragging && (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled))
-        {
-            dragging = false;
+            else
+            {
+                Debug.Log("Right");
+                player.transform.position=Vector3.Lerp(player.transform.position, player.transform.position + new Vector3(0, 0, -speed*Time.deltaTime), 1);
+            }
         }
     }
 
